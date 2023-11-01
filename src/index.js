0ecebddec1f44354b756133610030820e11a81a1
ryeeun <dmsdl7531@naver.com>
2023-11-01 23:18:58 +0900

    gh pages 설정

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/portfolio">
     <React.StrictMode>
       <App />
     </React.StrictMode>
